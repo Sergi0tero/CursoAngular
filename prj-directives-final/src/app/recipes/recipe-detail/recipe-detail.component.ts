20413ab62eda27d7ef9245baf500b06731161dad
Sergi0tero <sergioaotero26@hotmail.com>
2023-06-21 00:08:48 -0500

    first method to add recipe ingredients to shopping list

--- prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/services/shoppingList.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService : ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  addToShoppingList(){
+    this.recipe.ingredients.forEach(ingredient => {
+      this.shoppingListService.addIngredient(ingredient);
+    });
   }
 
 }
