2036ad93eb6eb2b2c8bc3bacbf322b2bba98e4bf
Sergi0tero <sergioaotero26@hotmail.com>
2023-09-04 12:21:19 -0500

    setting up project routes

---
+++ prj-directives-final/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from "@angular/router";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { NgModule } from "@angular/core";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const approutes : Routes = [
+    { path : '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path : 'recipes', component: RecipesComponent },
+    { path : 'shopping-list', component: ShoppingListComponent }
+]
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(approutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
