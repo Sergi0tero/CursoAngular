9022a2f2c6095e919dd575405df2d4afdeb1bf48
Sergi0tero <sergioaotero26@hotmail.com>
2023-10-23 13:17:42 -0500

    Using form data

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -11,6 +11,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  submitted = false;
+  user = {
+    username : '',
+    mail: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
   
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -35,6 +43,11 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.mail = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
   }
 }
