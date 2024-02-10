70c625a7b1ae7a84ffde6ba16609866f4878c910
Sergi0tero <sergioaotero26@hotmail.com>
2024-02-10 11:55:23 -0500

    handling errors

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
   isFetching = false;
   error = null;
+  private errorSub : Subscription;
 
   constructor(private postsService : PostsService) {}
 
   ngOnInit() {
     this.fetchPosts();
+  }
+
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
   }
 
   onCreatePost(postData: Post) {
@@ -41,13 +47,14 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts(){
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.isFetching = true;
     this.postsService.fetchPosts().subscribe(posts => {
       this.loadedPosts = posts;
       this.isFetching = false;
-    }, error => {
-      this.error = error.message;
-    });; 
+    });;
   }
 
 }
--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -2,9 +2,12 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
+    error = new Subject<string>();
+
     constructor(private http: HttpClient){}
 
     createAndStorePost(postData : Post){
@@ -23,6 +26,8 @@ export class PostsService{
                 }
             }
             return postArray; 
+        }, error => {
+            this.error.next(error.message);
         }))
     }
 
